2be89ae37c54959f6d68835757443a1dd25f90d2
Quien Sabe <qs5779@mail.com>
2020-01-06 13:29:09 -0500

    First commit.

---
+++ lib/foodie/food.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'active_support/inflector'
+
+module Foodie
+  #
+  # @class Food
+  #
+  class Food
+    def self.portray(food)
+      if food.downcase == 'broccoli'
+        'Gross!'
+      else
+        'Delicious!'
+      end
+    end
+
+    def self.pluralize(word)
+      word.pluralize
+    end
+  end
+end
